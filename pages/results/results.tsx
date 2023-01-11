dfd73d48756536be52de047eb5c53fcac985ad15
rhonamackay <rhonamackay@outlook.com>
2023-01-11 15:45:20 +0000

    Results page search section (no drop down functionality)

--- pages/results/results.tsx
+++ pages/results/results.tsx
@@ -1,5 +1,9 @@
 import ResultsHeader from "../../components/ResultsHeader"
+import ResultsSearchSection from "../../components/ResultsSearchSection"
 
 export default function Results() {
-return <ResultsHeader/>
+    return <>
+    <ResultsHeader/>
+    <ResultsSearchSection/>
+    </>
 }
