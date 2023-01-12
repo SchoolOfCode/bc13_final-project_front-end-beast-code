490b2c58af47c69f3152a20d43c313455631a59e
rhonamackay <rhonamackay@outlook.com>
2023-01-12 11:03:42 +0000

    Map functional with data

--- components/Map.tsx
+++ components/Map.tsx
@@ -4,29 +4,41 @@ import { TileLayer } from 'react-leaflet';
 import { Marker } from 'react-leaflet';
 import { Popup } from 'react-leaflet';
 import * as bardata from '../bar-data.json';
+import { useEffect, useState } from 'react';
 
-type barArray = {
-    City: string,
-    Name: string,
-    Cost: number,
-    Description: string,
-    Image: string,
-    Rating: number,
-    Address: string,
-    Postcode: string,
-    Hygiene: number,
-    Happy_hr: string,
-    Website: string,
-    Music: string[],
-    Venue_type: string[],
-    Other: string[],
-    Vibe: string[],
-    Features: string[],
-    Who_with: string[],
-    coordinates: [number, number]
-}[]
+type singleBarType = {
+    name: string, 
+    address: string,
+    postcode: string, 
+    cost: number,
+    rating: number, 
+    coordinates: [number,number],
+    website: string
+}
 
 export default function Map(){
+  const [barDetails, setBarDetails] = useState<singleBarType[]>([])   
+  
+  function getBarDetails() {
+    let tempBarDetailsArr = []
+    for (let i=0; i<bardata.length; i++) {
+      let barArray:singleBarType = {
+        name: bardata[i].Name,
+        address: bardata[i].Address,
+        postcode: bardata[i].Postcode,
+        cost: bardata[i].Cost,
+        rating: bardata[i].Rating,
+        coordinates: [bardata[i].location.coordinates[1], bardata[i].location.coordinates[0]],
+        website: bardata[i].Website
+      }
+    tempBarDetailsArr.push(barArray)  
+    }
+    setBarDetails(tempBarDetailsArr)
+  }
+  
+  useEffect(() => {
+    getBarDetails()
+  }, [])
 
 
     return (
@@ -35,13 +47,13 @@ export default function Map(){
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          {bardata.map((element) => <Marker key={element.Postcode} position={element.coordinates}>
+          {barDetails.map((element) => <Marker key={element.postcode} position={element.coordinates}>
             <Popup>
-              <h3>{element.Name}</h3>
-              <p>{element.Address}</p>
-              <p>Cost: {element.Cost}/3</p>
-              <p>Rating: {element.Rating}/5</p>
-              <a href={element.Website} rel="noreferrer" target="_blank">Website</a>
+              <h3>{element.name}</h3>
+              <p>{element.address}</p>
+              <p>Cost: {element.cost}/3</p>
+              <p>Rating: {element.rating}/5</p>
+              <a href={element.website} rel="noreferrer" target="_blank">Website</a>
             </Popup>
           </Marker>)}
         </MapContainer>
