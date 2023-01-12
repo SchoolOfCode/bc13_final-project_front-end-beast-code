40d17d8dbc0a3035b0ae9e1445ae027ad6e0457e
Faseeh Ahmed <faseeh_458@hotmail.com>
2023-01-12 19:08:55 +0000

    header font size changed, logo to go back to home page, logo added

--- components/ResultsHeader.tsx
+++ components/ResultsHeader.tsx
@@ -1,37 +1,50 @@
 import Link from "next/link";
-import styles from "../styles/ResultsHeader.module.css"
-import Image from "next/image"
+import styles from "../styles/ResultsHeader.module.css";
+import Image from "next/image";
 
-export default function ResultsHeader(){
-    return (
-      
-  <>
-  <div className={styles.Navbar}>
-    <div className={styles.logo} data-testid="logo">
-      <Image src='/../public/Logo.png' width={40} height={40} alt='logo'></Image>
-      <strong>Cheers</strong>
-    </div>
-    <div className={styles.search_container}>
-          <div data-testid="location-pin" className="logo-area"> location pin icon 
-          </div>
-          <input data-testid="location-input" placeholder="Location or Postcode" className={styles.location_input}></input>
+export default function ResultsHeader() {
+  return (
+    <>
+      <div className={styles.Navbar}>
+        <div className={styles.logo} data-testid="logo">
+          <Link href="/">
+            <Image
+              src="/../public/Logo.png"
+              width={33}
+              height={33}
+              alt="logo"
+            ></Image>
+          </Link>
+          <strong>Cheers</strong>
         </div>
-    <div className={styles.about_login}>
-      <div className={styles.about_us}>
-        <h3>
-          <a href="#" data-testid="about-us">
-            About us
-          </a>
-        </h3>
-      </div>
-      <div className={styles.login}>
-        <h3>
-          <a href="#" data-testid="login">
-            Login
-          </a>
-        </h3>
-      </div>
-    </div>
-  </div>
+        <div className={styles.align_input_left}>
+          <div className={styles.search_container}>
+            <div data-testid="location-pin" className={styles.logo_area}></div>
+            <input
+              data-testid="location-input"
+              placeholder="Location or Postcode"
+              className={styles.location_input}
+            ></input>
+          </div>
+        </div>
 
-</>)}
+        <div className={styles.about_login}>
+          <div className={styles.about_us}>
+            <h3>
+              <a href="#" data-testid="about-us">
+                About us
+              </a>
+            </h3>
+          </div>
+          <div className={styles.login}>
+            <h3>
+              <a href="#" data-testid="login">
+                Login
+              </a>
+            </h3>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
