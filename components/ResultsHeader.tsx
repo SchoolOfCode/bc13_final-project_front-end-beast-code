68afd5ba35e948540f9889ff6afc1997b68f5e54
gregrutnam <111754256+gregrutnam@users.noreply.github.com>
2023-01-12 12:15:02 +0100

    results header started

--- components/ResultsHeader.tsx
+++ components/ResultsHeader.tsx
@@ -1,21 +1,36 @@
 import Link from "next/link";
+import styles from "../styles/ResultsHeader.module.css"
+import Image from "next/image"
 
 export default function ResultsHeader(){
     return (
-      <div>
-        <div data-testid="logo">
-          <Link href="/">Logo</Link>
-          </div>
-        <div>
+      
+  <>
+  <div className={styles.Navbar}>
+    <div className={styles.logo} data-testid="logo">
+      <Image src='/../public/Logo.png' width={40} height={40} alt='logo'></Image>
+      <strong>Cheers</strong>
+    </div>
+    <div className={styles.search_container}>
           <div data-testid="location-pin"> location pin icon </div>
-          <input data-testid="location-input" placeholder="Location or Postcode"></input>
+          <input data-testid="location-input" placeholder="Location or Postcode" className={styles.location_input}></input>
         </div>
-        <a href="#" data-testid="about-us">
-          About us
-        </a>
-        <a href="#" data-testid="login">
-          Login
-        </a>
+    <div className={styles.about_login}>
+      <div className={styles.about_us}>
+        <h3>
+          <a href="#" data-testid="about-us">
+            About us
+          </a>
+        </h3>
       </div>
-    );
-  }
+      <div className={styles.login}>
+        <h3>
+          <a href="#" data-testid="login">
+            Login
+          </a>
+        </h3>
+      </div>
+    </div>
+  </div>
+
+</>)}
