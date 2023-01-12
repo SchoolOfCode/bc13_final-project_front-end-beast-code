131162eadbcad558cdb698017f7bc700e449aad4
Faseeh Ahmed <faseeh_458@hotmail.com>
2023-01-12 14:41:51 +0000

    results keyword search css done, about us button linked to results page

--- components/ResultsSearchSection.tsx
+++ components/ResultsSearchSection.tsx
@@ -1,29 +1,28 @@
-import React from 'react'
-import FilterDropdown from './FilterDropdown'
-import FilterPanel from "./FilterPanel"
-import { useState } from 'react'
-import styles from '../styles/Results-search-section.module.css'
-import Image from 'next/image'
+import React from "react";
+import FilterDropdown from "./FilterDropdown";
+import FilterPanel from "./FilterPanel";
+import { useState } from "react";
+import styles from "../styles/Results-search-section.module.css";
+import Image from "next/image";
 
 export default function ResultsSearchSection() {
-
-  const [toggle, setToggle] = useState(false)
+  const [toggle, setToggle] = useState(false);
 
   function showFilterPanel() {
-    console.log("HIIIIIIII")
-    setToggle(true)
+    console.log("HIIIIIIII");
+    setToggle(true);
   }
 
-  function hideFilterPanel(){
-    setToggle(false)
+  function hideFilterPanel() {
+    setToggle(false);
   }
 
   return (
     <div>
       {/* Search bar for filtering results by keyword */}
-      <div className={styles.keywordinput_container}>
-        <div className={styles.inner_div}>
-          <div data-testid={'search-icon'} className={styles.search_icon}></div>
+      <div className={styles.keywordsearch_main_container}>
+        <div className={styles.icon_input_toggle_container}>
+          <div data-testid={"search-icon"} className={styles.search_icon}></div>
           <input
             className={styles.keyword_input}
             type="text"
@@ -33,7 +32,7 @@ export default function ResultsSearchSection() {
           <button className={styles.map_button}>Map View</button>
           <button className={styles.list_button}>List View</button>
         </div>
-        <div className={styles.line}></div>
+        <div className={styles.gold_underline}></div>
       </div>
 
       {/* Map toggle button */}
