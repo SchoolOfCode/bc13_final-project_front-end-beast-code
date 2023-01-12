99b3547d5c3a72ccce344de63392c03e78711481
rhonamackay <rhonamackay@outlook.com>
2023-01-12 22:27:23 +0000

    css for result card partially done and some other small tweaks

--- components/BarCards.tsx
+++ components/BarCards.tsx
@@ -1,24 +1,40 @@
 import React from 'react'
 import ResultCard from './ResultCard'
 import { ResultCardProps } from './ResultCard'
+import styles from "../styles/resultcards.module.css"
 
 const DummyData = [
-{
-     "name": "Cosy Club",
-     "cost": 2,
-     "description": "Part of the iconic Corn Exchange, Manchester Cosy Club is a grand affair at the heart of the city centre. With many original historic features..."
-},
-{
-    "name": "Cosy Clubby",
-    "cost": 1,
-    "description": "Part of the iconic Corn Exchange, Manchester Cosy Club is a grand affair at the heart of the city centre. With many original historic features..."
-}
+  {
+      "name": "Cosy Club",
+      "cost": 2,
+      "description": "Part of the iconic Corn Exchange, Manchester Cosy Club is a grand affair at the heart of the city centre. With many original historic features..."
+  },
+  {
+      "name": "Cosy Clubby again",
+      "cost": 1,
+      "description": "Part of the iconic Corn Exchange, Manchester Cosy Club is a grand affair at the heart of the city centre. With many original historic features..."
+  },
+  {
+      "name": "Cosy Clubby",
+      "cost": 1,
+      "description": "Part of the iconic Corn Exchange, Manchester Cosy Club is a grand affair at the heart of the city centre. With many original historic features..."
+  },
+  {
+      "name": "Cosy Clubby",
+      "cost": 1,
+      "description": "Part of the iconic Corn Exchange, Manchester Cosy Club is a grand affair at the heart of the city centre. With many original historic features..."
+  },
+  {
+      "name": "Cosy Clubby ",
+      "cost": 1,
+      "description": "Part of the iconic Corn Exchange, Manchester Cosy Club is a grand affair at the heart of the city centre. With many original historic features..."
+  }
 ]
 
 function BarCards() {
-  return (<>
+  return (<div className={styles.cardContainer}>
     {DummyData.map((item: ResultCardProps, index: number)=><ResultCard key={index} name={item.name} cost={item.cost} description={item.description} />)}
-    </>
+    </div>
     
   )
 }
--- pages/results/results.tsx
+++ pages/results/results.tsx
@@ -1,12 +1,14 @@
 import BarCards from "../../components/BarCards"
 import ResultsHeader from "../../components/ResultsHeader"
 import ResultsSearchSection from "../../components/ResultsSearchSection"
+import styles from "../../styles/resultsPage.module.css"
+
 
 export default function Results() {
-    return <>
+    return <div className={styles.resultsPage}>
     <ResultsHeader/>
     <ResultsSearchSection/>
     <BarCards/>
     <button>Load More...</button>
-    </>
+    </div>
 }
