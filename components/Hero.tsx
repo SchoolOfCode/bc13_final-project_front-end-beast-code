335d51358b177fc30b5eee6530a20121aae60d15
Faseeh Ahmed <faseeh_458@hotmail.com>
2023-01-13 11:35:54 +0000

    css polishing

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,5 @@
 import styles from "../styles/hero.module.css";
+import Link from 'next/link'
 
 export default function Hero() {
   return (
@@ -6,10 +7,11 @@ export default function Hero() {
       <h1 data-testid="title">Raise the bar, sink into the atmosphere</h1>
       <div data-testid="search-container" className={styles.search_container}>
         <input placeholder="Location or Postcode" data-testid="input"></input>
-
-        <button data-testid="button">
-          <span>Search</span>
-        </button>
+        <Link href="/results/results">
+          <button data-testid="button" className={styles.search_button}>
+            <span>Search</span>
+          </button>
+        </Link>
       </div>
     </div>
   );
