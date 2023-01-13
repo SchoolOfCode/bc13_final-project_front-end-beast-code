f8eab6360d033910977231e092ba7b2c2b75d9ce
Faseeh Ahmed <faseeh_458@hotmail.com>
2023-01-13 08:58:57 +0000

    toggle animation added for list and map view

--- components/ResultsSearchSection.tsx
+++ components/ResultsSearchSection.tsx
@@ -3,8 +3,18 @@ import FilterDropdown from './FilterDropdown'
 import FilterPanel from "./FilterPanel"
 import styles from "../styles/Results-search-section.module.css";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function ResultsSearchSection() {
+  const [style, setStyle] = useState("btn");
+  function leftClick() {
+    setStyle("btn10");
+  }
+
+  function rightClick() {
+    setStyle("btn");
+  }
+
   return (
     <div>
       {/* Search bar for filtering results by keyword */}
@@ -17,21 +27,40 @@ export default function ResultsSearchSection() {
             placeholder="Search by keyword"
             data-testid="search-input"
           />
-          <button className={styles.map_button}>Map View</button>
-          <button className={styles.list_button}>List View</button>
+          <div className={styles.button_box}>
+            <div className={styles[style]}></div>
+            <button
+              type="button"
+              className={styles.toggle_btn}
+              onClick={(event: React.MouseEvent<HTMLElement>) => {
+                rightClick();
+              }}
+            >
+              List View
+            </button>
+            <button
+              type="button"
+              className={styles.toggle_btn}
+              onClick={(event: React.MouseEvent<HTMLElement>) => {
+                leftClick();
+              }}
+            >
+              Map View
+            </button>
+          </div>
         </div>
         <div className={styles.gold_underline}></div>
       </div>
-        {/* Map toggle button */}
-        <input type="checkbox" className="map_view_toggle"/>
-        <label htmlFor='map_view_toggle'>Map view toggle</label>
-        <>
+      {/* Map toggle button */}
+      <input type="checkbox" className="map_view_toggle" />
+      <label htmlFor="map_view_toggle">Map view toggle</label>
+      <>
         {/* Filter dropdown that should expand / close when clicked */}
-        <FilterDropdown/>
+        <FilterDropdown />
         {/* Filter panel should show when "filters" button is clicked */}
-        <FilterPanel/> 
-        </>
-        <button data-testid="reset-button">Reset</button>
+        <FilterPanel />
+      </>
+      <button data-testid="reset-button">Reset</button>
     </div>
   );
 }
