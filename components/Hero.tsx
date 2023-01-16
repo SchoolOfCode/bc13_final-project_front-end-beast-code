3df7673cf5babc119ec40dde2c9a333732609b49
keirastanley <keiraestanley@gmail.com>
2023-01-16 15:03:39 +0100

    User location input is accessible by results page and small change to instagram icon

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,18 +1,28 @@
 import styles from "../styles/hero.module.css";
+import { useState, useEffect, createContext } from "react";
 import Link from 'next/link'
 
 export default function Hero() {
+  const [location, setLocation] = useState("")
+
+  //SORT OUT TYPESCRIPT IF POSS
+  function getLocation(event : any) {
+    setLocation(event.target.value)
+  }
+
   return (
-    <div data-testid="main-container" className={styles.main_container}>
-      <h1 data-testid="title">Raise the bar, sink into the atmosphere</h1>
-      <div data-testid="search-container" className={styles.search_container}>
-        <input placeholder="Location or Postcode" data-testid="input"></input>
-        <Link href="/results/results">
-          <button data-testid="button" className={styles.search_button}>
-            <span>Search</span>
-          </button>
-        </Link>
+    <>
+      <div data-testid="main-container" className={styles.main_container}>
+        <h1 data-testid="title">Raise the bar, sink into the atmosphere</h1>
+        <div data-testid="search-container" className={styles.search_container}>
+          <input placeholder="Location or Postcode" data-testid="input" onChange={getLocation}></input>
+          <Link href={{pathname: "/results/results", query: {location: location}}}>
+            <button data-testid="button" className={styles.search_button}>
+              <span>Search</span>
+            </button>
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
