81be95eff75ce8c758bf99ed0f9491b63819cc2d
susannaclark <susannaclark95@gmail.com>
2023-01-24 14:14:23 +0000

    added linkedin and twitter icons, added description and linkedin links

--- components/AboutUsProfile.tsx
+++ components/AboutUsProfile.tsx
@@ -4,12 +4,32 @@ import aboutStyles from "../styles/aboutus.module.css";
 type propsAbout = {
     name: string, 
     tenSeconds: string,
+    linkedIn: string,
+    gitHub: string,
 }
 
-export function AboutUsProfile({name, tenSeconds}: propsAbout) {
+export function AboutUsProfile({name, tenSeconds, linkedIn, gitHub}: propsAbout) {
     return <div className={aboutStyles.Profiles}>
     <h3>{name}</h3>
     <Image src="/search-icon.png" width={32} height={32} alt="photo of team member"></Image>
     <p>{tenSeconds}</p>
+    <div>
+           <a href={linkedIn} target="_blank" rel="noreferrer">
+           <Image
+              src="/linkedin-logo.png"
+              alt={`${name} + 's LinkedIn profile`}
+              width={30}
+              height={30}
+            ></Image>
+            </a>
+            <a href={gitHub} target="_blank" rel="noreferrer">
+            <Image
+              src="/github-logo.png"
+              alt={`${name} + 's GitHub profile`}
+              width={30}
+              height={30}
+            ></Image>
+            </a>
+          </div>
     </div>
 }
--- pages/aboutus/index.tsx
+++ pages/aboutus/index.tsx
@@ -4,15 +4,15 @@ import {AboutUsProfile} from "../../components/AboutUsProfile";
 import BarPageHeader from "../../components/BarPageHeader";
 
 let team: teamType = [
-  {fullname: "Keira Stanley", tenSeconds: "I frequently visit Brazil and support Spurrs"},
-  {fullname: "Gregory Rutnam", tenSeconds: "Tabby tiggy would 100% beat Orange Tiggy in a fight"},
-  {fullname: "Faseeh Ahmed", tenSeconds: "Say it like you meme it"},
-  {fullname: "Suzi Clark", tenSeconds: "You're not wrong Greg"},
-  {fullname: "Rhona Mackay", tenSeconds: "Far be Knit for me to forget if I knit or crochet amazing jumpers, but I know I do one or the other..."},
-  {fullname: "Anthony Akinfoyeku", tenSeconds: "One day I will have a cat that might be as cool as Orange Tiggy but never as majestic as Tabby Tiggy"}
+  {fullname: "Keira Stanley", tenSeconds: "I frequently visit Brazil and support Spurrs", linkedIn: "https://www.linkedin.com/in/keirastanley/", gitHub: "/"},
+  {fullname: "Gregory Rutnam", tenSeconds: "Tabby tiggy would 100% beat Orange Tiggy in a fight", linkedIn: "https://www.linkedin.com/in/rutnam/", gitHub: "/"},
+  {fullname: "Faseeh Ahmed", tenSeconds: "Say it like you meme it", linkedIn: "https://www.linkedin.com/in/faseehahmed/", gitHub: "/"},
+  {fullname: "Suzi Clark", tenSeconds: "You're not wrong Greg", linkedIn: "https://www.linkedin.com/in/susanna-clark-932b52142/", gitHub: "/"},
+  {fullname: "Rhona Mackay", tenSeconds: "Far be Knit for me to forget if I knit or crochet amazing jumpers, but I know I do one or the other...", linkedIn: "https://www.linkedin.com/in/rhonamackay/", gitHub: "/"},
+  {fullname: "Remi Akinfoyeku", tenSeconds: "One day I will have a cat that might be as cool as Orange Tiggy but never as majestic as Tabby Tiggy", linkedIn: "https://www.linkedin.com/in/anthony-akinfoyeku-720846a6/", gitHub: "/"}
 ]
 
-type teamType = {fullname: string, tenSeconds: string}[]
+type teamType = {fullname: string, tenSeconds: string, linkedIn: string, gitHub: string}[]
 
 export default function AboutUs() {
   return (
@@ -20,10 +20,12 @@ export default function AboutUs() {
       <BarPageHeader/>
       <div className={styles.About_info}>
       <h1>About Us</h1>
+      <p>Our app, Cheers, removes the frustrations of needing to visit several websites and google to plan a night out - and still not finding all the information required to be sure a bar is right for you!<br/> 
+      Using different filters and search capabilities including a map feature, Cheers users have all the information they could possibly want at their fingertips.</p>
       <h2>Meet the &#39;Beast Code&#39; team who created this website:</h2>
       </div>
       <div className={styles.AllProfiles}>
-      {team.map((each) => (<AboutUsProfile key={each.fullname} name={each.fullname} tenSeconds={each.tenSeconds}/>))}
+      {team.map((each) => (<AboutUsProfile key={each.fullname} name={each.fullname} tenSeconds={each.tenSeconds} linkedIn={each.linkedIn} gitHub={each.gitHub}/>))}
       </div>
     </div>
   );
