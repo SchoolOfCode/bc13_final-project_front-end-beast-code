4100067415597fa55e97d56a60f660e10271b954
Faseeh Ahmed <faseeh_458@hotmail.com>
2023-01-24 22:28:08 +0000

    numbers added

--- pages/aboutus/index.tsx
+++ pages/aboutus/index.tsx
@@ -1,7 +1,8 @@
 import styles from "../../styles/aboutus.module.css";
-import Image from "next/image";
 import {AboutUsProfile} from "../../components/AboutUsProfile";
 import BarPageHeader from "../../components/BarPageHeader";
+import CountUp from "react-countup";
+import { useState } from "react";
 
 let team: teamType = [
   {fullname: "Keira Stanley", tenSeconds: "I frequently visit Brazil and support Spurrs", linkedIn: "https://www.linkedin.com/in/keirastanley/", gitHub: "https://github.com/keirastanley", profileImage: "/keira.png"},
@@ -15,25 +16,82 @@ let team: teamType = [
 type teamType = {fullname: string, tenSeconds: string, linkedIn: string, gitHub: string, profileImage: string}[]
 
 export default function AboutUs() {
+  const [counterOn, setCounterOn] = useState(false)
   return (
     <div className={styles.whole_page}>
-      <BarPageHeader/>
+      <BarPageHeader />
       <div className={styles.about_us}>
         <div className={styles.about_info}>
           <h1>Meet the Team</h1>
           <hr className={styles.lines}></hr>
         </div>
         <div className={styles.all_profiles}>
-          {team.map((each) => (<AboutUsProfile key={each.fullname} profileImage={each.profileImage} name={each.fullname} tenSeconds={each.tenSeconds} linkedIn={each.linkedIn} gitHub={each.gitHub}/>))}
+          {team.map((each) => (
+            <AboutUsProfile
+              key={each.fullname}
+              profileImage={each.profileImage}
+              name={each.fullname}
+              tenSeconds={each.tenSeconds}
+              linkedIn={each.linkedIn}
+              gitHub={each.gitHub}
+            />
+          ))}
         </div>
         <h1>The Project in Numbers</h1>
         <hr className={styles.lines}></hr>
-        <p>some fas number magic</p>
+        <div className={styles.all_stats}>
+          <div className={styles.stat1}>
+            {/* <span> </span> */}
+            <CountUp
+              start={0}
+              end={119}
+              delay={0}
+              duration={2}
+              className={styles.counter}
+            ></CountUp>
+            +<p>Pull requests on GitHub</p>
+          </div>
+          <div className={styles.stat2}>
+            {/* <span> </span> */}
+            <CountUp
+              start={0}
+              end={311}
+              delay={0}
+              duration={2}
+              className={styles.counter}
+            ></CountUp>
+            +<p>Commits on GitHub</p>
+          </div>
+          <div className={styles.stat3}>
+            {/* <span> </span> */}
+            <CountUp
+              start={0}
+              end={95}
+              delay={0}
+              duration={2}
+              className={styles.counter}
+            ></CountUp>
+            +<p>Trello ticked complete</p>
+          </div>
+          </div>
         <h1>What we built & Why</h1>
         <hr className={styles.lines}></hr>
-        <h3 className={styles.project_description}>For the final 4-week project of the SoC bootcamp our team took a problem-first approach to ideation:</h3>
-        <p className={styles.project_description}>We decided to create an app that solves the problem of needing to visit several websites and do a frustratingly large number of google searches to plan a night out - and still not finding all the information required to be sure a bar is right for you!</p>
-        <p className={styles.project_description}>Using different filters and search capabilities including a map feature, Cheers users have all the information they could possibly desire at their fingertips - no matter the group, occasion or features required.</p>
+        <h3 className={styles.project_description}>
+          For the final 4-week project of the SoC bootcamp our team took a
+          problem-first approach to ideation:
+        </h3>
+        <p className={styles.project_description}>
+          We decided to create an app that solves the problem of needing to
+          visit several websites and do a frustratingly large number of google
+          searches to plan a night out - and still not finding all the
+          information required to be sure a bar is right for you!
+        </p>
+        <p className={styles.project_description}>
+          Using different filters and search capabilities including a map
+          feature, Cheers users have all the information they could possibly
+          desire at their fingertips - no matter the group, occasion or features
+          required.
+        </p>
         <h1>Our Tech Stack</h1>
         <hr className={styles.lines}></hr>
         <div className={styles.tech_stack}>
